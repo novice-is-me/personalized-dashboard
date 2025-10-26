d9b8476c069f6f57ac0d69990121a7252e138bb5
novice-is-me <reginajalandoni13@gmail.com>
2025-10-26 21:24:56 +0800

    Structure and libraries

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import Dashboard from "./layouts/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+      <Button variant="">Click Me</Button>
+    </>
+  );
+}
+
+export default App;
